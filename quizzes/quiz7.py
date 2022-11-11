b821278555bb611e0d923079094a2dcd610ea491
Syxd Iltxfxt <40826265+ishtiq10001@users.noreply.github.com>
2022-11-11 07:45:06 -0500

    Add files via upload

--- quizzes/quiz7.py
+++ quizzes/quiz7.py
@@ -1,1 +1,31 @@
+from math import *
+from random import *
+# global dictionary caesars cyhper
+caesar = {'a':'x' , 'b':'y' ,'c':'z' ,'d':'a' ,'e':'b' ,'f':'c' ,'g':'d' ,'h':'e' ,'i':'f' ,'j':'g' ,'k':'h' ,'l':'i' ,'m':'j' ,'n':'k' ,'o':'l' ,'p':'m' ,'q':'n' ,'r':'o' ,'s':'p' ,'t':'q' ,'u':'r' ,'v':'s' ,'w':'t' ,'x':'u' ,'y':'v' ,'z':'w'}
 
+
+
+def decypher(a,b):
+   
+    for x in a:
+        for y in x:
+            y = y.lower()
+            for i,j in caesar.items():
+                if y == j:
+                    b +=i
+        b+="\n"
+            
+    print(b)
+
+
+
+
+if __name__ == "__main__":
+    
+
+    file_I = open('secret_code.txt', 'r')
+    secret_code = file_I.readlines()
+    file_I.close()
+    
+    
+    decypher(secret_code, '')
