a87b9424ba403bfed45696ba5cc21c7f2da6ca4f
Syxd Iltefat <syedishtiq00@gmail.com>
2022-11-16 18:12:42 -0500

    - added question 3 - added question 4 - added question 5

--- Lab 8/lab8.py
+++ Lab 8/lab8.py
@@ -2,7 +2,9 @@ from math import floor,ceil,pi
 from random import randint,random,randrange
 
 
-def add_rec(a,b):
+countcalls = 0
+
+def add_rec(a,b):#q1
     if b == 1:
         return a+1
 
@@ -10,22 +12,42 @@ def add_rec(a,b):
 
         return 1 + add_rec(a,b-1)
 
-def log2(x):
+def log2(x):#q2
     if x <= 1:
         return 0
     else:
         return 1+log2(x/2)
 
-def reverse(sentence):
-    pass
+def reverse(sentence):#q3
+    if len(sentence) <= 1:
+        return sentence
+    else:
+        return reverse(sentence[1:])+sentence[0]
+
+def power(x,n):#q4
+    global countcalls
+    if n == 0:
+        return 1
+    elif n < 0:
+        return None
+    if x == 0:
+        return 0
+    else:
+        countcalls +=1
+        return x * power(x,n-1)
 
 
-
-
-
-
-
-
+def powerHalf(x,n):
+    global countcalls
+    if n == 0:
+        return 1
+    elif n < 0:
+        return None
+    if x == 0:
+        return 0
+    else:
+        countcalls +=1
+        return x * power(x,n-1)
 
 
 
@@ -42,4 +64,11 @@ def reverse(sentence):
 if __name__ == "__main__":
     print(add_rec(5,9))
     print(log2(23))
-    print()
+    print(reverse('sentence'))
+    #print(power(3,9))
+    print(f"2**10 = {power(2,10)}, recursion called: {countcalls} times")
+    countcalls = 0
+    print(f"5**10 = {power(5,10)}, recursion called: {countcalls} times")
+    countcalls = 0
+    print(f"5**0 = {power(5,0)}, recursion called: {countcalls} times")
+    countcalls = 0
