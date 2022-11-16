39af41035c5e44db424c80b4a73d7d02d2aef4b1
Syxd Iltefat <syedishtiq00@gmail.com>
2022-11-16 18:54:59 -0500

    -improved question 6 -added question 7 -added kdpF.txt

--- Lab 8/lab8.py
+++ Lab 8/lab8.py
@@ -45,30 +45,74 @@ def powerHalf(x,n):
         return None
     if x == 0:
         return 0
+
+    elif n%2==0:
+        countcalls +=1
+        return ((powerHalf(x,n/2))**2)
+
     else:
         countcalls +=1
-        return x * power(x,n-1)
+        return x * powerHalf(x,n-1)
 
 
+def inputSeq():
+    f = open('kdpF.txt') # opens a file for reading
+    line = f.readline() # reads a single line
+    #print(line)
+    seq = ''
+    for line in f : # reading the rest of the lines
+        seq = seq + line
+    seq = seq.replace('\n', '') # removing the newline characters
+    seq = seq.upper()
+
+    return seq
 
 
+def gcContent(sequence):
+    totalG = 0
+    totalC = 0
+    totalSeq = 0
+    for x in sequence:
+        if x.isupper() == True:
+            totalSeq+=1
 
+    for p in sequence:
+        if p == 'G':
+            totalG +=1
+        elif p == 'C':
+            totalC +=1
+        else:
+            pass
 
+    percG = (totalG/totalSeq) * 100
+    percC = (totalC/totalSeq) * 100
 
-
+    print(f"percentage of G: {percG}")
+    print(f"percentage of C: {percC}")
 
 
 
 
 
 if __name__ == "__main__":
-    print(add_rec(5,9))
-    print(log2(23))
-    print(reverse('sentence'))
-    #print(power(3,9))
+    print(add_rec(5,9)) #question 1
+    print(log2(23)) #question2
+    print(reverse('sentence'))#question 3
+    print(power(3,9))#question 4
+    #question 5
     print(f"2**10 = {power(2,10)}, recursion called: {countcalls} times")
     countcalls = 0
     print(f"5**10 = {power(5,10)}, recursion called: {countcalls} times")
     countcalls = 0
-    print(f"5**0 = {power(5,0)}, recursion called: {countcalls} times")
+    print(f"5**0 = {power(5,0)}, recursion called: {countcalls} times\n")
     countcalls = 0
+
+    #question 6
+    print(f"\n2**10 = {powerHalf(2,10)}, recursion called: {countcalls} times")
+    countcalls = 0
+    print(f"5**10 = {powerHalf(5,10)}, recursion called: {countcalls} times")
+    countcalls = 0
+    print(f"5**0 = {powerHalf(5,0)}, recursion called: {countcalls} times")
+    countcalls = 0
+
+    gcContent(inputSeq()) #question 7
